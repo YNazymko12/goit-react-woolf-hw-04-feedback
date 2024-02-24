108662dc5f43cd79cfb4797a0524cdc2a1ae9836
YNazymko12 <122815071+YNazymko12@users.noreply.github.com>
2024-02-24 14:46:02 +0000

    Deploying to gh-pages from @ YNazymko12/goit-react-woolf-hw-04-feedback@b05b9eaf5730657a373739af47272889792b09ac 🚀

--- src/components/Section/Section.styled.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const SectionWrapper = styled.section`
-  margin: 0 auto;
-`;
-export const Title = styled.h2`
-  text-align: center;
-  font-size: 36px;
-`;
