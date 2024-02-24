0a32c64447b1cf9b3a2bf8c88b5035211e2cf08b
Yuliia Nazymko <122815071+YNazymko12@users.noreply.github.com>
2024-02-24 12:32:00 +0100

    Initial commit

---
+++ src/components/Section/Section.styled.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const SectionWrapper = styled.section`
+  margin: 0 auto;
+`;
+export const SectionTitle = styled.h2`
+  text-align: center;
+  font-size: 36px;
+`;
